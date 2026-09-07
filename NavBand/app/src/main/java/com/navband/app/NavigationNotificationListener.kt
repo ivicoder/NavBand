a03d983413608d6f3ef64c540477e55aff009ca6
ivicoder <iviprova7@gmail.com>
2026-09-07 10:08:20 +0000

    Prepare NavBand Android project

--- NavBand/app/src/main/java/com/navband/app/NavigationNotificationListener.kt
+++ NavBand/app/src/main/java/com/navband/app/NavigationNotificationListener.kt
@@ -1,81 +1,17 @@
 package com.navband.app
 
-import android.app.Notification
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 
-class NavigationNotificationListener :
-    NotificationListenerService() {
+class NavigationNotificationListener : NotificationListenerService() {
 
-    private lateinit var forwarder:
-        NotificationForwarder
+    override fun onNotificationPosted(sbn: StatusBarNotification?) {
+        super.onNotificationPosted(sbn)
 
-    override fun onCreate() {
-
-        super.onCreate()
-
-        forwarder =
-            NotificationForwarder(this)
+        // In seguito inseriremo qui la logica di NavBand.
     }
 
-    override fun onNotificationPosted(
-        sbn: StatusBarNotification
-    ) {
-
-        if (!isNavigationApp(sbn.packageName)) {
-            return
-        }
-
-        val notification =
-            sbn.notification ?: return
-
-        val extras =
-            notification.extras ?: return
-
-        val title =
-            extras
-                .getCharSequence(
-                    Notification.EXTRA_TITLE
-                )
-                ?.toString()
-
-        val text =
-            extras
-                .getCharSequence(
-                    Notification.EXTRA_TEXT
-                )
-                ?.toString()
-
-        val subText =
-            extras
-                .getCharSequence(
-                    Notification.EXTRA_SUB_TEXT
-                )
-                ?.toString()
-
-        val image =
-            ImageExtractor.extract(
-                notification
-            )
-
-        val event =
-            NavigationParser.parse(
-                title = title,
-                text = text,
-                subText = subText,
-                image = image
-            ) ?: return
-
-        forwarder.send(event)
-    }
-
-    private fun isNavigationApp(
-        packageName: String
-    ): Boolean {
-
-        return packageName ==
-            "com.google.android.apps.maps" ||
-            packageName ==
-            "com.waze"
+    override fun onNotificationRemoved(sbn: StatusBarNotification?) {
+        super.onNotificationRemoved(sbn)
     }
 }
