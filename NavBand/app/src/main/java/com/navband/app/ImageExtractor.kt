22f01deed1dd43d47d222dd4eee068ec10541a1c
ivicoder <iviprova7@gmail.com>
2026-09-07 11:03:35 +0000

    Simplify ImageExtractor

--- NavBand/app/src/main/java/com/navband/app/ImageExtractor.kt
+++ NavBand/app/src/main/java/com/navband/app/ImageExtractor.kt
@@ -2,53 +2,22 @@ package com.navband.app
 
 import android.app.Notification
 import android.graphics.Bitmap
-import android.os.Build
-import android.os.Bundle
 
 object ImageExtractor {
 
     fun extract(notification: Notification): Bitmap? {
-        val extras: Bundle = notification.extras ?: return null
+        val extras = notification.extras ?: return null
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        val picture = extras.get(Notification.EXTRA_PICTURE)
 
-            val picture = extras.getParcelable(
-                Notification.EXTRA_PICTURE,
-                Bitmap::class.java
-            )
+        if (picture is Bitmap) {
+            return picture
+        }
 
-            if (picture != null) {
-                return picture
-            }
+        val icon = extras.get(Notification.EXTRA_LARGE_ICON)
 
-            val icon = extras.getParcelable(
-                Notification.EXTRA_LARGE_ICON,
-                Bitmap::class.java
-            )
-
-            if (icon != null) {
-                return icon
-            }
-
-        } else {
-
-            @Suppress("DEPRECATION")
-            val picture = extras.getParcelable(
-                Notification.EXTRA_PICTURE
-            )
-
-            if (picture is Bitmap) {
-                return picture
-            }
-
-            @Suppress("DEPRECATION")
-            val icon = extras.getParcelable(
-                Notification.EXTRA_LARGE_ICON
-            )
-
-            if (icon is Bitmap) {
-                return icon
-            }
+        if (icon is Bitmap) {
+            return icon
         }
 
         return null
