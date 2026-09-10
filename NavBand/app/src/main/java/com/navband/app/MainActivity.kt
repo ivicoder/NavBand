f51da4a2c08b50a2ad413d3f6e993e91b3850835
ivicoder <iviprova7@gmail.com>
2026-09-10 11:11:18 +0000

    Fix Color import for bitmap notification test

--- NavBand/app/src/main/java/com/navband/app/MainActivity.kt
+++ NavBand/app/src/main/java/com/navband/app/MainActivity.kt
@@ -27,7 +27,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.unit.dp
 
