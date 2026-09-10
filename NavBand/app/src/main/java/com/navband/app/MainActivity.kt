11a7031dfa1ade81220758ee4881e0cc66b2e6d5
ivicoder <iviprova7@gmail.com>
2026-09-10 12:04:29 +0000

    Fix Compose color import conflict

--- NavBand/app/src/main/java/com/navband/app/MainActivity.kt
+++ NavBand/app/src/main/java/com/navband/app/MainActivity.kt
@@ -214,7 +214,7 @@ class MainActivity : ComponentActivity() {
                                 contentDescription =
                                     "Immagine estratta da Google Maps",
                                 modifier = Modifier
-                                    .background(Color.BLACK)
+                                    .background(androidx.compose.ui.graphics.Color.Black)
                                     .padding(10.dp)
                             )
 
